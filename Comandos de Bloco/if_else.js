aa7da69db7535f3a07a77b2e89e0108290fadb31
Elton <elton.lucas@dce.ufpb.br>
2020-09-21 15:05:19 -0300

    first commit

---
+++ Comandos de Bloco/if_else.js
@@ -0,0 +1,22 @@
+//declarando variaveis e atribuindo valores
+
+var a = 5;
+var b = 6; 
+
+if (a > b){
+
+    console.log(a + ' é maior que ' + b); //o simbolo '+' concatena a variavel com a string
+
+}
+
+else {
+
+    if (a < b){
+        console.log(a + ' é menor que ' + b);
+    }
+
+    else{
+        console.log(a + 'é igual a' + b);
+    }
+
+}
